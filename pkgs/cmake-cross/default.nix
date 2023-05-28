a8b34321cc46b43d5e7fff9b2d7eaf3ec3b5d9da
jszymanski <jacek.p.szymanski@gmail.com>
2023-05-28 08:41:53 +0200

    docs: document the need to add libs in crosscompilation with mings

--- pkgs/cmake-cross/default.nix
+++ pkgs/cmake-cross/default.nix
@@ -1,5 +1,8 @@
 { lib, stdenv, pkgs }:
 
+/* TODO: this adds include and lib folders to search paths, but since windows libraries are
+   not distributed as dlls with mingw, but as .a files, it's still needed to add them to
+   linker arguments. */
 with pkgs;
 cmake.overrideAttrs (oldAttrs: {
   pname = "cmake-cross";
