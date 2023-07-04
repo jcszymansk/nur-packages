a4f5c68a102f45d6f4cded44033de4e83619709e
jacek szymanski <jacek.p.szymanski@gmail.com>
2023-07-04 18:28:12 +0200

    fix: apache url

--- pkgs/apache-maven/default.nix
+++ pkgs/apache-maven/default.nix
@@ -3,7 +3,7 @@
 pkgs.maven.overrideAttrs (final: prev: rec {
   version = "3.9.2";
   src = builtins.fetchurl {
-    url = "https://dlcdn.apache.org/maven/maven-3/${version}/binaries/${prev.pname}-${version}-bin.tar.gz";
+    url = "https://archive.apache.org/dist/maven/maven-3/${version}/binaries/${prev.pname}-${version}-bin.tar.gz";
     sha256 = "0z8i6k07n5l3xgcczib6lp5qwkfklswlqckcq2ar25vd1hig77l0";
   };
 })
