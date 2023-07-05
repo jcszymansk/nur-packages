a6844c273ffa925fb0c7f3cb8c3b518be7994537
jacek szymanski <jacek.p.szymanski@gmail.com>
2023-07-05 06:53:39 +0200

    chore: apache maven 3.9.3

--- pkgs/apache-maven/default.nix
+++ pkgs/apache-maven/default.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 
 pkgs.maven.overrideAttrs (final: prev: rec {
-  version = "3.9.2";
+  version = "3.9.3";
   src = builtins.fetchurl {
     url = "https://archive.apache.org/dist/maven/maven-3/${version}/binaries/${prev.pname}-${version}-bin.tar.gz";
-    sha256 = "0z8i6k07n5l3xgcczib6lp5qwkfklswlqckcq2ar25vd1hig77l0";
+    sha256 = "1wd4km8n4kxvzd73azax4nw85xnf5rjzqzy503cn8frgqk03mqg1";
   };
 })
