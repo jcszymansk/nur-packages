96dbee5b6598938c370dcd2738b5c055957c2c78
jacek szymanski <jacek.p.szymanski@gmail.com>
2024-06-25 08:19:37 +0200

    fix: simplify vscode, no direct <nixpkgs>

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -1,19 +1,8 @@
-{ nixpkgs
-, stdenv
-, pkgs
-, lib
+{ stdenv
+, vscode
 , fetchurl
-, callPackage
-, makeDesktopItem
-, commandLineArgs ? ""
-, useVSCodeRipgrep ? stdenv.isDarwin
 , ... } @ args:
 
-/*
- * cannot just override because the fhs function captures the original
- * derivation, not the overriden one, so won't get FHS version of the
- * insiders' build.
- */
 let
   inherit (stdenv.hostPlatform) system;
   throwSystem = throw "Unsupported system: ${system}";
@@ -32,66 +21,10 @@ let
     url = data.url;
     sha256 = data.sha256hash;
   };
-  executableName = if stdenv.isDarwin then "code" else "code-insiders";
-  longName = "Visual Studio Code - Insiders";
-  shortName = "Code - Insiders";
-  defaultDesktopItem = makeDesktopItem {
-    name = executableName;
-    desktopName = longName;
-    comment = "Code Editing. Redefined.";
-    genericName = "Text Editor";
-    exec = "${executableName} %F";
-    icon = "code";
-    startupNotify = true;
-    startupWMClass = shortName;
-    categories = [ "Development" "IDE" ];
-    mimeTypes = [ ];
-    keywords = [ "vscode" ];
-    actions.new-empty-window = {
-      name = "New Empty Window";
-      exec = "${executableName} --new-window %F";
-      icon = "code";
-    };
-  };
 in
-  (callPackage "${nixpkgs}/pkgs/applications/editors/vscode/generic.nix" {
-    inherit version src executableName;
-    pname = "vscode-insiders";
-
-    inherit longName shortName;
-    inherit commandLineArgs useVSCodeRipgrep;
-
-    # We don't test vscode on CI, instead we test vscodium
-    tests = {};
-
-    sourceRoot = "";
-
-    updateScript = ./update.sh;
-
-    # Editing the `code` binary within the app bundle causes the bundle's signature
-    # to be invalidated, which prevents launching starting with macOS Ventura, because VS Code is notarized.
-    # See https://eclecticlight.co/2022/06/17/app-security-changes-coming-in-ventura/ for more information.
-    dontFixup = stdenv.isDarwin;
-
-    meta = with lib; {
-      description = ''
-        Open source source code editor developed by Microsoft for Windows,
-        Linux and macOS
-      '';
-      mainProgram = executableName;
-      longDescription = ''
-        Open source source code editor developed by Microsoft for Windows,
-        Linux and macOS. It includes support for debugging, embedded Git
-        control, syntax highlighting, intelligent code completion, snippets,
-        and code refactoring. It is also customizable, so users can change the
-        editor's theme, keyboard shortcuts, and preferences
-      '';
-      homepage = "https://code.visualstudio.com/";
-      downloadPage = "https://code.visualstudio.com/Updates";
-      license = licenses.unfree;
-      maintainers = [ "jacekszymanski" ];
-      platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" "armv7l-linux" ];
-    };
+  (vscode.override {
+    isInsiders = true;
   }).overrideAttrs (_: {
-    desktopItem = args.desktopItem or defaultDesktopItem;
+    inherit src version;
+    pname = "vscode-insiders";
   })
