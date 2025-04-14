d7633b082727f705a86db6c79ecffa30798d7436
jacek szymanski <jacek.p.szymanski@gmail.com>
2025-04-15 00:00:00 +0200

    fix: wrap aider-desk, chmod +w config

--- pkgs/aider-desk/default.nix
+++ pkgs/aider-desk/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, appimageTools, ... }:
+{ lib, stdenv, fetchurl, appimageTools, makeWrapper, coreutils, ... }:
 
 let
   pname = "aider-desk";
@@ -9,10 +9,37 @@ let
     sha256 = "16pmzl9llr0zf7rvapk0n5kicgpwb50van9kmn4w3b887rq4hfrq";
   };
 
+  aider-desk-raw = appimageTools.wrapType2 {
+    inherit pname version src;
+    extraPkgs = pkgs: with pkgs; [
+      python3
+    ];
+
+    nativeBuildInputs = [ makeWrapper ];
+
+    postInstall = ''
+      wrapProgram $out/bin/${pname} --set AIDER_DESK_NO_AUTO_UPDATE true
+    '';
+  };
 in
-appimageTools.wrapType2 {
-  inherit pname version src;
-  extraPkgs = pkgs: with pkgs; [
-    python3
-  ];
+stdenv.mkDerivation {
+  pname = "aider-desk-wrapper";
+  inherit version;
+
+  dontUnpack = true;
+  dontPatch = true;
+  dontConfigure = true;
+  dontBuild = true;
+  dontCheck = true;
+  dontFixup = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    ln -s ${aider-desk-raw}/bin/${pname} $out/bin/${pname}
+    wrapProgram $out/bin/${pname} \
+      --set AIDER_DESK_NO_AUTO_UPDATE true \
+      --run "${coreutils}/bin/chmod -R u+w \''${XDG_CONFIG_HOME:-\$HOME/.config}/${pname}" \
+  '';
 }
