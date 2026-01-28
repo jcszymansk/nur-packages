e3453a9d11fc67be2f3677facfb61efb47c657a1
jacek szymanski <jacek.p.szymanski@gmail.com>
2026-01-29 00:00:00 +0100

    chore: update ralphy

--- pkgs/ralphy/default.nix
+++ pkgs/ralphy/default.nix
@@ -9,11 +9,11 @@
 
 buildNpmPackage rec {
   pname = "ralphy";
-  version = "4.5.3";
+  version = "4.7.1";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/ralphy-cli/-/ralphy-cli-${version}.tgz";
-    sha256 = "/hwKwlsIOplkk+4HltPHynwJo3/kY5l6UGATTAAKA5o=";
+    sha256 = "sha256-tuwCz3LgMOU7TkA2cBwLE6RPRZEZ6dlAD7Qzd2ikSTA=";
   };
 
   npmDepsHash = "sha256-K8QoMjoFoTWusOknhXU//r0roVNx8JdBuQNfUplv6Fg=";
